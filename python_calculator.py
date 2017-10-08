8460fa13b85f8a0ad0e594c79158e3852e353ef0
Artyom Artemyev <artyomartemyev962015@gmail.com>
2017-10-08 16:52:09 +0300

    added simple application - calculator

---
+++ python_calculator.py
@@ -0,0 +1,15 @@
+def calculator():
+    print("Calculator")
+    print("Enter a:")
+    a = input()
+    print("Enter b:")
+    b = input()
+    print(int(a) +  int(b))
+    print(int(a) / int(b))
+    print(int(a) - int(b))
+    print(int(a) * int(b))
+
+if __name__ == '__main__':
+    calculator()
+
+
